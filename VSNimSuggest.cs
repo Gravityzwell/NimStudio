39df3390c8a492d4ae72fed077e618a68c3db199
Gravityzwell <Gravityzwell@users.noreply.github.com>
2015-03-23 08:44:20 -0400

    Nimsuggest updates

--- VSNimSuggest.cs
+++ VSNimSuggest.cs
@@ -11,12 +11,12 @@ using con = System.Console;
 namespace NimStudio.NimStudio {
 
     public class NimSuggestProc {
-        public Process proc = null;
+        private Process proc = null;
         public List<string> conout = new List<string>();
-        //public List<string> sugs = new List<string>();
         public List<List<string>> sugs = new List<List<string>>();
-        public Thread thread = null;
-        public bool queryfinished = false;
+        //private Thread thread = null;
+        private bool queryfinished = false;
+        private string filepath_prev="";
 
         static NimSuggestProc() {
             // static constructor
@@ -26,6 +26,38 @@ namespace NimStudio.NimStudio {
             // instance constructor
         }
 
+
+        public void query(string filepath) {
+            conout.Clear();
+            sugs.Clear();
+            queryfinished = false;
+            proc.StandardInput.WriteLine(str);
+            int waitcount = 0;
+            while (!queryfinished) {
+                Thread.Sleep(50);
+                waitcount++;
+                if (waitcount > 100) {
+                    // 5 second wait cap
+                    queryfinished = true;
+                    break;
+                }
+            }
+            foreach (string qstr in conout) {
+                string[] qwords = qstr.Split(new char[] { '\t' });
+                //List<string> qwords = new List<string>(qstr.Split(new Char[] { '\t' }));
+                if (qwords.Length < 2) break;
+                if (qwords[1] == "skProc" || qwords[1] == "skVar") {
+                    //new List<string>(new string[]{"G","H","I"})
+                    qwords[2] = qwords[2].Replace("htmlarc.", "");
+                    qwords[7] = qwords[7].Replace(@"\x0D\x0A", "\n");
+                    qwords[7] = qwords[7].Trim(new char[] { '"' });
+                    if (qwords[7] != "")
+                        qwords[7] = "\n\n" + qwords[7];
+                    sugs.Add(new List<string>(new string[] { qwords[2], qwords[3] + qwords[7] }));
+                }
+            }
+        }
+
         public void conwrite(string str) {
             conout.Clear();
             sugs.Clear();
@@ -33,17 +65,20 @@ namespace NimStudio.NimStudio {
             proc.StandardInput.WriteLine(str);
             int waitcount=0;
             while (!queryfinished) {
+                if (proc == null || proc.HasExited) break;
                 Thread.Sleep(50);
                 waitcount++;
-                if (waitcount > 100) { 
-                    // 5 second wait cap
-                    queryfinished=true;
+                if (waitcount > 100) // 5 second wait cap
                     break;
-                }
+            }
+            queryfinished = true;
+            if (proc == null || proc.HasExited) {
+                proc=null;
+                filepath_prev="";
+                return;
             }
             foreach (string qstr in conout) {
                 string[] qwords = qstr.Split(new char[] { '\t' });
-                //List<string> qwords = new List<string>(qstr.Split(new Char[] { '\t' }));
                 if (qwords.Length < 2) break;
                 if (qwords[1] == "skProc" || qwords[1] == "skVar") {
                     //new List<string>(new string[]{"G","H","I"})
@@ -73,15 +108,21 @@ namespace NimStudio.NimStudio {
             proc.StartInfo.UseShellExecute = false;
 
             proc.EnableRaisingEvents = true;
-            proc.OutputDataReceived += new DataReceivedEventHandler(DRHandler);
-            proc.ErrorDataReceived += new DataReceivedEventHandler(DRHandler);
+            proc.OutputDataReceived += new DataReceivedEventHandler(NSDRHandler);
+            proc.ErrorDataReceived += new DataReceivedEventHandler(NSDRHandler);
+            proc.Exited += new EventHandler(NSExitHandler);
             proc.Start();
             proc.BeginOutputReadLine();
             proc.StandardInput.WriteLine("");
             Thread.Sleep(300); // allow time for nimsuggest startup lines to be processed
         }
 
-        private void DRHandler(object sender, DataReceivedEventArgs e) {
+        private void NSExitHandler(object sender, EventArgs e) {
+            proc.Dispose();
+            proc = null;
+        }
+
+        private void NSDRHandler(object sender, DataReceivedEventArgs e) {
             string line = e.Data;
             if (line == "")
                 queryfinished=true;
